2951ad2b24ae5a32978c84017657895137592b50
Andrew LaPointe <andrewrlapointe@gmail.com>
2022-03-22 13:57:59 -0600

    added slideshow stickynav

--- decksanddecks/javascript/stickynav.js
+++ decksanddecks/javascript/stickynav.js
@@ -1,5 +1,7 @@
 // When the user scrolls the page, execute myFunction
+// console.log("jsfile");
 window.onscroll = function () {
+  //   console.log("onscroll");
   myFunction();
 };
 
@@ -11,7 +13,7 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  console.log("word");
+  //   console.log("ranfunc");
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
   } else {
